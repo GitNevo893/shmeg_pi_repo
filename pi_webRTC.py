0b5371f8eecc5e8188a0a7c345a2ee19720d74a5
GitNevo893 <warrior.sharkies@gmail.com>
2026-02-05 13:21:25 +0200

    Refactor WebSocket connection to use async function
    Refactor to define an asynchronous function for signaling and connect to the server.

--- pi_webRTC.py
+++ pi_webRTC.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
-async with websockets.connect("wss://YOUR_RENDER_URL") as ws:
-    await ws.send("hello")
-    message = await ws.recv()
+import asyncio
+import websockets
+
+# This is an ASYNCHRONOUS function
+async def signaling_test():
+    # Connect to your signaling server
+    async with websockets.connect("wss://YOUR_RENDER_URL") as ws:
+        print("Connected to signaling server")
+
+        # Send a test message
+        await ws.send("hello from raspberry pi")
+
+        # Wait for a message back
+        message = await ws.recv()
+        print("Received:", message)
+
+# This starts the async function properly
+asyncio.run(signaling_test())
