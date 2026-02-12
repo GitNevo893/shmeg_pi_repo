bcbcbfc29ba3b8dae7d13a465179464b3c8ae868
GitNevo893 <warrior.sharkies@gmail.com>
2026-02-12 12:51:23 +0200

    Update pi_webRTC.py

--- pi_webRTC.py
+++ pi_webRTC.py
@@ -17,7 +17,7 @@ config = RTCConfiguration(
     iceServers=[
         RTCIceServer(urls="stun:stun.l.google.com:19302"),
         RTCIceServer(
-            urls="turn:openrelay.metered.ca:80",
+            urls="turn:openrelay.metered.ca:443?transport=tcp",
             username="openrelayproject",
             credential="openrelayproject"
         )
